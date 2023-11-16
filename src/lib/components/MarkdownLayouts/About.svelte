076b2dcecb4419ce7f6e7e3a17f9f5e8cec6ac5d
achtaitaipai <charles.cailleteau@gmail.com>
2023-11-16 14:44:04 +0100

    feat: about background

--- src/lib/components/MarkdownLayouts/About.svelte
+++ src/lib/components/MarkdownLayouts/About.svelte
@@ -2,6 +2,8 @@
 	import { browser } from '$app/environment'
 	import Title from '../Title.svelte'
 	import Toc from '../Toc.svelte'
+	import { citys, width, height } from '$lib/93.json'
+	import { getPathFromPoints } from '$lib/utils/getPathFromPoints.js'
 
 	/**@type{string}*/
 	export let title
@@ -10,6 +12,22 @@
 <svelte:head>
 	<title>À propos</title>
 </svelte:head>
+
+<div class="fixed inset-0 -z-50 p-16 text-center">
+	<svg
+		role="figure"
+		viewBox={`0 0 500 500`}
+		class="  mx-auto mr-2 inline-block max-h-full max-w-full blur-sm"
+	>
+		{#each citys as { code, points }}
+			<path
+				class="fill-gray-900 stroke-blue/40 stroke-[2] transition-opacity duration-500"
+				d={getPathFromPoints(points, width, height)}
+				stroke-linejoin="round"
+			/>
+		{/each}
+	</svg>
+</div>
 
 <Title text={title} />
 <div class="container mx-auto mt-12 flex flex-row-reverse justify-center">
@@ -21,7 +39,7 @@
 		</aside>
 	{/if}
 
-	<article class="prose prose-lg prose-invert col-start-2 w-full grow px-4 pb-40">
+	<article class="prose prose-lg prose-invert col-start-2 w-full grow px-4 xl:pb-40">
 		<slot />
 	</article>
 </div>
