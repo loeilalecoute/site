5580fc1af2c5fe0e38b0b6f1a67a39c0b7916d36
achtaitaipai <charles.cailleteau@gmail.com>
2023-11-26 16:00:04 +0100

    add images

--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,5 +1,21 @@
+<script>
+</script>
+
 <svelte:head>
 	<title>Portfolio</title>
 </svelte:head>
 
 <h1 class="text-3xl">Portfolio</h1>
+
+<ul class="grid grid-cols-4 gap-8">
+	<li><img src="/images/0.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/1.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/2.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/3.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/4.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/5.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/6.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/7.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/8.jpeg" alt="An alt text" /></li>
+	<li><img src="/images/9.jpeg" alt="An alt text" /></li>
+</ul>
