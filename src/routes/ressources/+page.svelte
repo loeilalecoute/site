25fb3afd0cd5eea4739302476842f4aa79651230
achtaitaipai <charles.cailleteau@gmail.com>
2023-12-02 10:44:05 +0100

    remove italic

--- src/routes/ressources/+page.svelte
+++ src/routes/ressources/+page.svelte
@@ -50,7 +50,7 @@
 			<a
 				href="https://lessoeurschevalme.ultra-book.com/"
 				rel="external"
-				class="font-bold italic transition-colors hover:text-yellow focus-visible:text-yellow"
+				class="font-bold transition-colors hover:text-yellow focus-visible:text-yellow"
 				>les Soeurs Chevalme</a
 			>
 		</div>
