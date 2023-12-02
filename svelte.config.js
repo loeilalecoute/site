b272900d7a23601c094f8c7ec368793458b075c7
achtaitaipai <charles.cailleteau@gmail.com>
2023-12-02 13:53:22 +0100

    ignore http errors from audioassets

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, _, message }) => {
+				// ignore deliberate link to shiny 404 page
+				if (path.startsWith('/audioassets/')) {
+					return
+				}
+
+				// otherwise fail the build
+				throw new Error(message)
+			}
+		}
 	},
 	preprocess: [
 		mdsvex({
