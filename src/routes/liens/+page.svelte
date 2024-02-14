8b69b70231943f93e3d02ffa3a95bbf95d6d2b9a
achtaitaipai <charles.cailleteau@gmail.com>
2024-02-14 13:11:23 +0100

    fix icons before links

--- src/routes/liens/+page.svelte
+++ src/routes/liens/+page.svelte
@@ -18,46 +18,57 @@
 				<a
 					rel="external"
 					href="mailto:contact@loeilalecoute.fr"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 24 24"
+						class="mr-2 inline-block"
 						><path
 							fill="currentColor"
 							d="M4 20q-.825 0-1.413-.588T2 18V6q0-.825.588-1.413T4 4h16q.825 0 1.413.588T22 6v12q0 .825-.588 1.413T20 20H4Zm8-7L4 8v10h16V8l-8 5Zm0-2l8-5H4l8 5ZM4 8V6v12V8Z"
 						/></svg
-					>
-
-					contact@loeilalecoute.fr</a
+					> contact@loeilalecoute.fr</a
 				>
 			</li>
 			<li>
 				<a
 					rel="external"
 					href="tel:0672612564"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 256 256"
+						class="mr-2 inline-block"
 						><path
 							fill="currentColor"
 							d="m222.37 158.46l-47.11-21.11l-.13-.06a16 16 0 0 0-15.17 1.4a8.12 8.12 0 0 0-.75.56L134.87 160c-15.42-7.49-31.34-23.29-38.83-38.51l20.78-24.71c.2-.25.39-.5.57-.77a16 16 0 0 0 1.32-15.06v-.12L97.54 33.64a16 16 0 0 0-16.62-9.52A56.26 56.26 0 0 0 32 80c0 79.4 64.6 144 144 144a56.26 56.26 0 0 0 55.88-48.92a16 16 0 0 0-9.51-16.62ZM176 208A128.14 128.14 0 0 1 48 80a40.2 40.2 0 0 1 34.87-40a.61.61 0 0 0 0 .12l21 47l-20.67 24.74a6.13 6.13 0 0 0-.57.77a16 16 0 0 0-1 15.7c9.06 18.53 27.73 37.06 46.46 46.11a16 16 0 0 0 15.75-1.14a8.44 8.44 0 0 0 .74-.56L168.89 152l47 21.05h.11A40.21 40.21 0 0 1 176 208Z"
 						/></svg
-					>
-					Guillaume Mahe: 06 72 61 25 64</a
+					>Guillaume Mahe: 06 72 61 25 64</a
 				>
 			</li>
 			<li>
 				<a
 					rel="external"
 					href="tel:0602105847"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 256 256"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 256 256"
+						class="mr-2 inline-block"
 						><path
 							fill="currentColor"
 							d="m222.37 158.46l-47.11-21.11l-.13-.06a16 16 0 0 0-15.17 1.4a8.12 8.12 0 0 0-.75.56L134.87 160c-15.42-7.49-31.34-23.29-38.83-38.51l20.78-24.71c.2-.25.39-.5.57-.77a16 16 0 0 0 1.32-15.06v-.12L97.54 33.64a16 16 0 0 0-16.62-9.52A56.26 56.26 0 0 0 32 80c0 79.4 64.6 144 144 144a56.26 56.26 0 0 0 55.88-48.92a16 16 0 0 0-9.51-16.62ZM176 208A128.14 128.14 0 0 1 48 80a40.2 40.2 0 0 1 34.87-40a.61.61 0 0 0 0 .12l21 47l-20.67 24.74a6.13 6.13 0 0 0-.57.77a16 16 0 0 0-1 15.7c9.06 18.53 27.73 37.06 46.46 46.11a16 16 0 0 0 15.75-1.14a8.44 8.44 0 0 0 .74-.56L168.89 152l47 21.05h.11A40.21 40.21 0 0 1 176 208Z"
 						/></svg
-					>
-					Hervé Bouchet: 06 02 10 58 47</a
+					>Hervé Bouchet: 06 02 10 58 47</a
 				>
 			</li>
 		</ul>
@@ -67,8 +78,13 @@
 				<a
 					href="https://www.facebook.com/loeilalecoute"
 					target="_blank"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
-					><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
+					><svg
+						class="mr-2 inline-block"
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 24 24"
 						><path
 							fill="none"
 							stroke="currentColor"
@@ -77,17 +93,21 @@
 							stroke-width="1.5"
 							d="M17 2h-3a5 5 0 0 0-5 5v3H6v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3V2Z"
 						/></svg
-					>
-					Facebook</a
+					>Facebook</a
 				>
 			</li>
 			<li>
 				<a
 					href="http://association-loeilalecoute.blogspot.com"
 					target="_blank"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 2048 2048"
+						class="mr-2 inline-block"
 						><path
 							fill="currentColor"
 							d="M2048 0v1536H731l-475 475v-475H0V0h2048zM128 128v256h1792V128H128zm1792 1280V512H128v896h256v293l293-293h1243zm-640-768h512v640h-512V640zm128 512h256V768h-256v384zM256 768h896v128H256V768zm0 256h896v128H256v-128z"
@@ -99,30 +119,37 @@
 				<a
 					href="https://www.radiocampusparis.org/emission/v2m-loeil-a-lecoute"
 					target="_blank"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
-					><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
+					><svg
+						class="mr-2 inline-block"
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 24 24"
 						><path
 							fill="currentColor"
 							d="M11 22v-8.275q-.45-.275-.725-.712T10 12q0-.825.588-1.413T12 10q.825 0 1.413.588T14 12q0 .575-.275 1.025t-.725.7V22h-2Zm-5.9-2.75q-1.425-1.375-2.262-3.238T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.15-.838 4.025T18.9 19.25l-1.4-1.4q1.15-1.1 1.825-2.613T20 12q0-3.35-2.325-5.675T12 4Q8.65 4 6.325 6.325T4 12q0 1.725.675 3.225t1.85 2.6L5.1 19.25Zm2.825-2.825q-.875-.825-1.4-1.963T6 12q0-2.5 1.75-4.25T12 6q2.5 0 4.25 1.75T18 12q0 1.325-.525 2.475t-1.4 1.95L14.65 15q.625-.575.988-1.35T16 12q0-1.65-1.175-2.825T12 8q-1.65 0-2.825 1.175T8 12q0 .9.363 1.663T9.35 15l-1.425 1.425Z"
 						/></svg
-					>
-
-					Liens vers l'intégralité des podcasts sur Radio Campus Paris</a
+					>Liens vers l'intégralité des podcasts sur Radio Campus Paris</a
 				>
 			</li>
 			<li>
 				<a
 					href="http://association-loeilalecoute.blogspot.com/p/presse.html"
 					target="_blank"
-					class="flex items-center gap-1 transition-colors hover:text-yellow focus-visible:text-yellow"
+					class="transition-colors hover:text-yellow focus-visible:text-yellow"
 				>
-					<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="1em"
+						height="1em"
+						viewBox="0 0 16 16"
+						class="mr-2 inline-block"
 						><path
 							fill="currentColor"
 							d="M3.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1h-7Zm0 2a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-2ZM4 9V8h1v1H4Zm3.5-2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3Zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3ZM1 4a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2a2 2 0 0 1 2 2v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 10.5V4Zm11.5 6.5a.5.5 0 0 1-.5-.5V4a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v6.5A1.5 1.5 0 0 0 3.5 12h9a1.5 1.5 0 0 0 1.5-1.5V6a1 1 0 0 0-1-1v5a.5.5 0 0 1-.5.5Z"
 						/></svg
-					>
-					Revue de presse (2005-2015)</a
+					>Revue de presse (2005-2015)</a
 				>
 			</li>
 		</ul>
